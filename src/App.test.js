2e63324d808cdfa923a7b99c63fd18041db7d43e
Jeff Ubri <jeffubri@gmail.com>
2021-11-17 17:10:05 -0500

    delete useless files

--- src/App.test.js
+++ src/App.test.js
@@ -13,3 +13,28 @@ beforeEach(async () => {
 afterEach(async () => {
   await browser.close();
 });
+
+it("header appears", async () => {
+  const text = await page.$eval("h2", (el) => el.innerHTML);
+  expect(text).toEqual("Enter your Sudoku puzzle");
+});
+
+it("input changes value", async () => {
+  await page.focus("input");
+  await page.keyboard.type("1");
+  const text = await page.$eval("input", (el) => el.value);
+  expect(text).toEqual("1");
+});
+
+it("input default to regular class", async () => {
+  await page.focus("input");
+  const text = await page.$eval("input", (el) => el.className);
+  expect(text).toEqual("regular");
+});
+
+it("input changes class", async () => {
+  await page.focus("input");
+  await page.keyboard.type("1");
+  const text = await page.$eval("input", (el) => el.className);
+  expect(text).toEqual("onChange");
+});
