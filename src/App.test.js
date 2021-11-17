f2cabf0f3e2bfafb27bddf5c35319fffe4bb4e00
Jeff Ubri <jeffubri@gmail.com>
2021-11-17 17:06:42 -0500

    add jest

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+const puppeteer = require("puppeteer");
+
+let browser;
+let page;
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
